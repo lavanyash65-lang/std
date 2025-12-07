59808e6fa964cafdd32223c3bdc4138e07ad1d93
lavanyash65-lang <lavanyash65@gmail.com>
2025-12-07 23:49:49 +0530

    Update test_std.py

--- test_std.py
+++ test_std.py
@@ -2,9 +2,9 @@ from std import student_details
 
 def test_student_details():
     expected_output = (
-        "Student Name: Bob\n "
-        "Roll Number: R102\n "
-        "Course: BSc CS\n "
+        "Student Name: Bob\n"
+        "Roll Number: R102\n"
+        "Course: BSc CS\n"
         "Marks: 87"
-                      )
+    )
     assert student_details("Bob", "R102", "BSc CS", 87) == expected_output
