b1664f7043fc5ce17e117501cec5524e49c65f15
lavanyash65-lang <lavanyash65@gmail.com>
2025-12-07 23:39:37 +0530

    Update test_std.py

--- test_std.py
+++ test_std.py
@@ -6,5 +6,5 @@ def test_student_details():
         "Roll Number: R102\n "
         "Course: BSc CS\n "
         "Marks: 87"
-    )
+                      )
     assert student_details("Bob", "R102", "BSc CS", 87) == expected_output
