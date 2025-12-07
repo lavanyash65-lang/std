22c0024988f84eeb45311114f61e503c820436d2
lavanyash65-lang <lavanyash65@gmail.com>
2025-12-07 23:30:12 +0530

    Add test for student_details function

---
+++ test_std.py
@@ -0,0 +1,10 @@
+from std import student_details
+
+def test_student_details():
+    expected_output = (
+        "Student Name: Bob\n "
+        "Roll Number: R102\n "
+        "Course: BSc CS\n "
+        "Marks: 87"
+    )
+    assert student_details("Bob", "R102", "BSc CS", 87) == expected_output
